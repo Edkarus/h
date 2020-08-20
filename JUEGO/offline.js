9a3df6f47c2e63292d22a7158578127ad27a1508
Edkarus <69987841+Edkarus@users.noreply.github.com>
2020-08-20 15:28:18 -0400

    Add files via upload

---
+++ JUEGO/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1597950055,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/person-sheet0.png",
+		"images/piso-sheet0.png",
+		"images/arbol-sheet0.png",
+		"images/nuve.png",
+		"images/coins-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
